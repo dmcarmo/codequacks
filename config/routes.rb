a5aa7e93d8b1f53d945e8255284262db7bfe351a
David Carmo <dmcarmo@gmail.com>
2020-06-22 17:09:13 +0100

    Created the Duck model

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: 'pages#home'
-  GET '/ducks', to: 'ducks#index'
-  GET '/ducks/new', to: 'ducks#new'
-  POST '/ducks', to: 'ducks#create'
-  POST '/ducks/:id/bookings', to: 'bookings#create'
-  GET '/ducks/:id', to: 'ducks#show'
-  GET '/dashboard', to: 'pages#dashboard'
-  GET '/bookings', to: 'bookings#index'
-  POST '/bookings/:id/reviews', to: 'reviews#create'
-  GET '/bookings/:id', to: 'bookings#show'
+  # GET '/ducks', to: 'ducks#index'
+  # GET '/ducks/new', to: 'ducks#new'
+  # POST '/ducks', to: 'ducks#create'
+  # POST '/ducks/:id/bookings', to: 'bookings#create'
+  # GET '/ducks/:id', to: 'ducks#show'
+  # GET '/dashboard', to: 'pages#dashboard'
+  # GET '/bookings', to: 'bookings#index'
+  # POST '/bookings/:id/reviews', to: 'reviews#create'
+  # GET '/bookings/:id', to: 'bookings#show'
 end
 
 
---
+++ db/migrate/20200622160209_create_ducks.rb
@@ -0,0 +1,12 @@
+class CreateDucks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :ducks do |t|
+      t.string :name
+      t.text :description
+      t.integer :price
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
