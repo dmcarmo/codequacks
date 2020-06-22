d7e41dd5153e6539dbe14d1b051a31be4cf60971
David Carmo <dmcarmo@gmail.com>
2020-06-22 17:31:01 +0100

    Reactivated the routes and fixed the verbs case

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: 'pages#home'
-  # GET '/ducks', to: 'ducks#index'
-  # GET '/ducks/new', to: 'ducks#new'
-  # POST '/ducks', to: 'ducks#create'
-  # POST '/ducks/:id/bookings', to: 'bookings#create'
-  # GET '/ducks/:id', to: 'ducks#show'
-  # GET '/dashboard', to: 'pages#dashboard'
-  # GET '/bookings', to: 'bookings#index'
-  # POST '/bookings/:id/reviews', to: 'reviews#create'
-  # GET '/bookings/:id', to: 'bookings#show'
+  get '/ducks', to: 'ducks#index'
+  get '/ducks/new', to: 'ducks#new'
+  post '/ducks', to: 'ducks#create'
+  post '/ducks/:id/bookings', to: 'bookings#create'
+  get '/ducks/:id', to: 'ducks#show'
+  get '/dashboard', to: 'pages#dashboard'
+  get '/bookings', to: 'bookings#index'
+  post '/bookings/:id/reviews', to: 'reviews#create'
+  get '/bookings/:id', to: 'bookings#show'
 end
 
 
