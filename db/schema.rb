21cc088201fd927e1be69420670094f4d396ef02
David Carmo <dmcarmo@gmail.com>
2020-06-22 17:22:58 +0100

    Created the Review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_22_161119) do
+ActiveRecord::Schema.define(version: 2020_06_22_161628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,16 @@ ActiveRecord::Schema.define(version: 2020_06_22_161119) do
     t.index ["user_id"], name: "index_ducks_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.bigint "booking_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -52,4 +62,6 @@ ActiveRecord::Schema.define(version: 2020_06_22_161119) do
   add_foreign_key "bookings", "ducks"
   add_foreign_key "bookings", "users"
   add_foreign_key "ducks", "users"
+  add_foreign_key "reviews", "bookings"
+  add_foreign_key "reviews", "users"
 end
