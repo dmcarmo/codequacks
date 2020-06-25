73ff969136363e6afcf4b887491a4ff7b08390f7
Silvia Grill <grillsilvia@msn.com>
2020-06-25 12:12:36 +0100

    created the bookings controller and nested the routes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,37 @@
 class BookingsController < ApplicationController
+
+
+
   def index
     @bookings = policy_scope(Booking)
     # @bookings = Booking.where(user: current_user)
-    
+  end
+
+  def new
+    @booking = Booking.new
+    authorize @booking
+    @duck = Duck.find(params[:duck_id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @duck = Duck.find(params[:duck_id])
+    @booking.duck = @duck
+    @booking.user = curent_user
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_time, :end_time)
   end
 end
