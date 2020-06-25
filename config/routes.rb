a4767e931d2c1e3c2707c2b0d7200f525457b185
Isaline Wethli <isalinewethli@hotmail.com>
2020-06-25 13:25:04 +0100

    reviews form added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :bookings do
+    resources :reviews
+  end
+
   devise_for :users
   root to: 'pages#home'
   get '/ducks', to: 'ducks#index'
@@ -8,9 +12,7 @@ Rails.application.routes.draw do
   post '/ducks/:id/bookings', to: 'bookings#create'
   get '/ducks/:id', to: 'ducks#show', as: :duck
   get '/dashboard', to: 'pages#dashboard'
-  get '/bookings', to: 'bookings#index'
-  post '/bookings/:id/reviews', to: 'reviews#create'
-  get '/bookings/:id', to: 'bookings#show'
+
 end
 
 
